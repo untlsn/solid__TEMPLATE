94b8fc592fb405062c1141dba6ce7a3be6b87e40
Filip Skoczen <philipskoczen@gmail.com>
2022-07-15 19:33:18 +0200

    Add Eslint

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          <span>Edit </span>
+          <code>src/App.tsx</code>
+          <span> and save to reload.</span>
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
